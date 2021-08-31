66ff900b06965fbd6ec3cd24883cdf7d1faa293c
Guldeniz Ozdemir <=guldenizozdemir03@gmail.com>
2021-08-31 15:47:00 +0300

    fixed checkbox 1

--- app/src/main/java/com/example/finalmyphrasalverbsproject/adapters/LessonAdapter.java
+++ app/src/main/java/com/example/finalmyphrasalverbsproject/adapters/LessonAdapter.java
@@ -1,12 +1,15 @@
 package com.example.finalmyphrasalverbsproject.adapters;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -14,16 +17,20 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.finalmyphrasalverbsproject.R;
 import com.example.finalmyphrasalverbsproject.models.Lesson;
 
+import java.nio.file.FileSystemNotFoundException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class LessonAdapter extends RecyclerView.Adapter<LessonAdapter.CardHolder> {
 
     ArrayList<Lesson> mDataList;
     LayoutInflater inflater;
+    List<String> selectedValues;
 
     public LessonAdapter(Context context, ArrayList<Lesson> mLessonList) {
         inflater = LayoutInflater.from(context);
         this.mDataList = mLessonList;
+        selectedValues = new ArrayList<>();
     }
 
     @NonNull
@@ -35,13 +42,40 @@ public class LessonAdapter extends RecyclerView.Adapter<LessonAdapter.CardHolder
     }
 
     @Override
-    public void onBindViewHolder(@NonNull CardHolder holder, int position) {
+    public void onBindViewHolder(@NonNull CardHolder holder, @SuppressLint("RecyclerView") int position) {
+        Lesson lesson = mDataList.get(position);
         holder.txt_lessonName.setText(mDataList.get(position).getLessonName());
+        final String al = String.valueOf(mDataList.get(position));
+        holder.checkBox_favorite.setText(al);
+        holder.checkBox_favorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (holder.checkBox_favorite.isChecked()){
+                    selectedValues.add(al);
+                    Log.d("Deniz", "position: " + position);
+                    deleteItem(position);
+                    Log.d("Deniz", "posizyon" + position);
+                }
+                else{
+                    selectedValues.remove(al);
+                    Toast.makeText(inflater.getContext(), "iptal edildi", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
     }
 
-    public void moveItem(int position, Lesson itemtogo){
-        Intent intent = new Intent();
+    public void deleteItem(int position){
         mDataList.remove(position);
+        notifyItemRemoved(position);
+        notifyItemRangeChanged(position, mDataList.size());
+
+        //notifyDataSetChanged();
+    }
+    public void addItem(int position, Lesson lessonToBeCopied){
+        mDataList.add(position, lessonToBeCopied);
+        notifyItemInserted(position);
     }
     @Override
     public int getItemCount() {
@@ -61,7 +95,6 @@ public class LessonAdapter extends RecyclerView.Adapter<LessonAdapter.CardHolder
             super(itemView);
             txt_lessonName = itemView.findViewById(R.id.txt_lessonNameGround);
             checkBox_favorite = itemView.findViewById(R.id.checkbox_favorite_verb);
-
             if (checkBox_favorite.isChecked()){
 
             }
